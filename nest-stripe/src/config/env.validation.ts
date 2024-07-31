2057b2446d234caa2415939695f35b6a3bce2350
Breno da Silva Ocanha <breno@dmxdesign.com.br>
2024-07-31 19:59:21 -0300

    nest-stripe -> feat: all env validations

--- nest-stripe/src/config/env.validation.ts
+++ nest-stripe/src/config/env.validation.ts
@@ -8,6 +8,9 @@ import {
 } from 'class-validator';
 
 class EnvironmentVariables {
+
+  // Database configuration
+
   @IsString()
   @IsNotEmpty()
   DATABASE_HOST: string;
@@ -33,6 +36,43 @@ class EnvironmentVariables {
     /^postgresql:\/\/([a-zA-Z0-9_\-]+):([a-zA-Z0-9_\-]+)@([a-zA-Z0-9.\-]+):(\d+)\/([a-zA-Z0-9_\-]+)$/,
   )
   DATABASE_URL: string;
+
+  // Stripe configuration (payment gateway)
+
+  @IsString()
+  @IsNotEmpty()
+  STRIPE_API_KEY: string;
+
+  // Secrets & Challenges
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_SECRET: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_REFRESH_TOKEN_SECRET: string;
+
+  // Email configuration
+  @IsString()
+  @IsNotEmpty()
+  EMAIL_HOST: string;
+
+  @IsString()
+  @IsNotEmpty()
+  EMAIL_PORT: string;
+
+  @IsString()
+  @IsNotEmpty()
+  EMAIL_USERNAME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  EMAIL_PASSWORD: string;
+
+  @IsString()
+  @IsNotEmpty()
+  EMAIL_FROM: string;
 }
 
 export function validate(config: Record<string, unknown>) {
